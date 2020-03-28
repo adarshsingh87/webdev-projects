5e459088e9a6e5f11d40c3eb1eb0580b64af12e2
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-03-29 00:27:43 +0530

    First react dev

---
+++ made using react/Robo_friends/min_robo_friends(React)/precache-manifest.338daf6a598392524cec69376dd930b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45173c5c95ebbd5959ed1d2bdb84c8f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e125e74c9d780b08167d",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "501003aa6cbdb4e78ad7",
+    "url": "/static/css/main.3862a353.chunk.css"
+  },
+  {
+    "revision": "e125e74c9d780b08167d",
+    "url": "/static/js/2.b1836917.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1836917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501003aa6cbdb4e78ad7",
+    "url": "/static/js/main.d78b35aa.chunk.js"
+  },
+  {
+    "revision": "9577190f5027890523fb",
+    "url": "/static/js/runtime-main.2c426d72.js"
+  }
+]);
