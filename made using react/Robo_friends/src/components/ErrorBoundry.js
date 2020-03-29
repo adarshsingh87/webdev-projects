967c5f7e15a4d22c594596bc04fda7abcef01e72
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-03-30 00:22:29 +0530

    fixed error from ErrorBoundry.js

--- made using react/Robo_friends/src/components/ErrorBoundry.js
+++ made using react/Robo_friends/src/components/ErrorBoundry.js
@@ -1,13 +1,13 @@
-import React,{components}from 'react';
-function ErrorBoundry(){
+import React,{ Component }from 'react';
+class ErrorBoundry extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			hasError=false
+			hasError:false
 		}
 	}
 	componentDidCatch(error,info){
-		this.state({hasError:true})
+		this.setState({hasError:true})
 	}
 	render() {
 		if(this.state.hasError){
